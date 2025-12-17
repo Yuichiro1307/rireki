977863529f776be291822cf5663e01bcdedb6b29
yuichiro1307 <yuichirou20130707@gmail.com>
2025-12-17 11:28:13 +0900

    JS

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'clock-cache-v1';
+const CACHE_FILES = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
